a18599ccfc040005b5072d4d48d908c52100d7a8
Jérémie Tabet <jtabet@users.noreply.github.com>
2019-10-22 14:59:37 +0200

    Added urls and tokens from config to the npm config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+# Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
+NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG:-"$HOME/.npmrc"}"
+
 if [ -n "$NPM_AUTH_TOKEN" ]; then
-  # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
-  NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG:-"$HOME/.npmrc"}"
   NPM_REGISTRY_URL="${NPM_REGISTRY_URL:-registry.npmjs.org}"
   NPM_STRICT_SSL="${NPM_STRICT_SSL:-true}"
   NPM_REGISTRY_SCHEME="https"
@@ -12,10 +13,27 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
   then
     NPM_REGISTRY_SCHEME="http"
   fi
-
+  
   # Allow registry.npmjs.org to be overridden with an environment variable
   printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG"
+  
+  chmod 0600 "$NPM_CONFIG_USERCONFIG"
+fi
 
+if [ -n "$NPM_AUTH_TOKEN_0" ]; then
+  i=0
+  eval currentToken=\${NPM_AUTH_TOKEN_$i}
+  eval currentUrl=\${NPM_REGISTRY_URL_$i}
+  touch "$NPM_CONFIG_USERCONFIG"
+
+  while [ ! -z "$currentToken" ] && [ ! -z "$currentUrl" ]; do
+    printf "//%s/:_authToken=%s\\n" "$currentUrl" "$currentToken" >> "$NPM_CONFIG_USERCONFIG"
+
+    i=$((i+1))
+    eval currentToken=\${NPM_AUTH_TOKEN_$i}
+    eval currentUrl=\${NPM_REGISTRY_URL_$i}
+  done
+  
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
 
